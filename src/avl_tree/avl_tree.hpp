485f4b83e82e57c8766f186208e37399c6b3ed85
DoroninIvan <ivan.doronin2018@yandex.ru>
2023-02-05 22:09:55 +0300

    set - fixed delete in avl

--- src/avl_tree/avl_tree.hpp
+++ src/avl_tree/avl_tree.hpp
@@ -17,7 +17,6 @@ namespace s21 {
         using difference_type = std::ptrdiff_t;
         using reference = value_type&;
         using node_alloc = typename Allocator::template rebind<s21::AVLNode<T>>::other;
-        using pair_alloc = Allocator;
 
         using key = typename value_type::first_type;
         using value = typename value_type::second_type;
@@ -25,6 +24,7 @@ namespace s21 {
     public:
         AVLTree() : root_(nullptr), size_(0) {}
         // AVLTree(const AVLTree &src) {}               --- COPY CTOR
+        ~AVLTree() {}
 
         bool empty() const S21_NOEXCEPT {
             return (size_ == 0);
@@ -41,9 +41,68 @@ namespace s21 {
         bool contains(key src) const S21_NOEXCEPT {
             return contains(root_, src);
         }
+
+        void delete_node(s21::AVLNode<value_type> *node) {
+            n_alloc.deallocate(node, 1);
+            node = NULL;
+        }
+
+        s21::AVLNode<value_type> *delete_subtree(s21::AVLNode<value_type> *node) {
+            if (node != nullptr) {
+                delete_subtree(node->left_);
+                delete_subtree(node->right_);
+                delete_node(node);
+            }
+
+            return nullptr;
+        }
+
+        void delete_tree() {
+            root_ = delete_subtree(root_);
+            size_ = 0;
+        }
+
+        s21::AVLNode<value_type> *create_node(value_type src) {
+            s21::AVLNode<value_type>* node = n_alloc.allocate(1);
+            n_alloc.construct(node, src);
+            return node;
+        }
+
+        bool insert(value_type src) {
+            if (!contains(root_, src.first)) {
+                root_ = insert(root_, src);
+                root_->parent_ = nullptr; size_++;
+                return true;
+            }
+
+            return false;
+        }
+
+////shiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiit
+
+        void print_help(s21::AVLNode<value_type> *node, int space) {
+            if (node == nullptr) {
+                return;
+            }
+
+            space += 10;
+
+            print_help(node->right_, space);
+            std::cout << "\n";
+            for (int i = 0; i < space; i++) {
+                std::cout << " ";
+            }
+            std::cout << node->data_.first << " " << node->data_.second << "\n";
+
+            print_help(node->left_, space);
+        }
+
+        void print() {
+            print_help(root_, 0);
+        }
     
     private:
-        int height(s21::AVLNode<value_type> *node) const S21_NOEXCEPT{
+        int height(s21::AVLNode<value_type> *node) const S21_NOEXCEPT {
             if (node == nullptr) {
                 return -1;
             }
@@ -58,8 +117,8 @@ namespace s21 {
                 return false;
             }
 
-            bool right_comp = comp_(node.data_.first, src);
-            bool left_comp = comp_(src, node->data_.right, src);
+            bool right_comp = comp_(src, node->data_.first);
+            bool left_comp = comp_(node->data_.first, src);
 
             if (!right_comp && !left_comp) {
                 return true;
@@ -68,7 +127,91 @@ namespace s21 {
             } else if (!left_comp) {
                 return (contains(node->right_, src));
             }
-            
+
+            return true;
+        }
+
+        s21::AVLNode<value_type>* insert(s21::AVLNode<value_type> *node, value_type src) S21_NOEXCEPT {
+            if (node == nullptr) {
+                return create_node(src);
+            }
+
+            bool cmp_result = comp_(src.first, node->data_.first);
+            if (cmp_result) {
+                node->left_ = insert(node->left_, src);
+                node->left_->parent_ = node;
+            } else if (!cmp_result) {
+                node->right_ = insert(node->right_, src);
+                node->right_->parent_ = node;
+            }
+
+            node_update(node);
+            return(balance(node));
+        }
+
+        void node_update(s21::AVLNode<value_type> *node) S21_NOEXCEPT {
+            int left_height = (node->left_ == nullptr) ? -1 : node->left_->height_;
+            int right_height = (node->right_ == nullptr) ? -1 : node->right_->height_;
+            node->height_ = 1 + std::max(left_height, right_height);
+            node->balance_factor_ = right_height - left_height;
+        }
+
+        s21::AVLNode<value_type> *balance(s21::AVLNode<value_type> *node) {
+            if (node->balance_factor_ == -2) {
+                if (node->left_->balance_factor_ <= 0) {
+                    return left_small_rotation(node);
+                } else {
+                    return left_big_rotation(node);
+                }
+            } else if (node->balance_factor_ == 2) {
+                if (node->right_->balance_factor_ >= 0) {
+                    return right_small_rotation(node); 
+                } else {
+                    return right_big_rotation(node);
+                }
+            }
+
+            return node;
+        }
+
+        s21::AVLNode<value_type> *left_small_rotation(s21::AVLNode<value_type> *node) S21_NOEXCEPT {
+            return right_rotation(node);
+        }
+
+        s21::AVLNode<value_type> *left_big_rotation(s21::AVLNode<value_type> *node) S21_NOEXCEPT {
+            node->left_ = left_rotation(node->left_);
+            return left_small_rotation(node);
+        }
+
+        s21::AVLNode<value_type> *right_small_rotation(s21::AVLNode<value_type> *node) S21_NOEXCEPT {
+            return left_rotation(node);
+        }
+
+        s21::AVLNode<value_type> *right_big_rotation(s21::AVLNode<value_type> *node) S21_NOEXCEPT {
+            node->right_ = right_rotation(node->right_);
+            return right_small_rotation(node);
+        }
+
+        s21::AVLNode<value_type> *left_rotation(s21::AVLNode<value_type> *node) S21_NOEXCEPT {
+            s21::AVLNode<value_type> *transplant_node = node->right_;
+            node->right_ = transplant_node->left_;
+            transplant_node->left_ = node;
+
+            //reset_parents(node, transplant_node);
+            node_update(node);
+            node_update(transplant_node);
+            return transplant_node;
+        }
+
+        s21::AVLNode<value_type> *right_rotation(s21::AVLNode<value_type> *node) S21_NOEXCEPT {
+            s21::AVLNode<value_type> *transplant_node = node->left_;
+            node->left_ = transplant_node->right_;
+            transplant_node->right_ = node;
+
+            //reset_parents(node, transplant_node);
+            node_update(node);
+            node_update(transplant_node);
+            return transplant_node;
         }
 
 
@@ -76,11 +219,8 @@ namespace s21 {
         s21::AVLNode<value_type> *root_;
         size_type size_;
         node_alloc n_alloc;
-        pair_alloc p_alloc;
         Cmp comp_;
-
-
     };
 }
 
-#endif 
+#endif  // AVL_TREE_HPP_
--- src/avl_tree/main.cpp
+++ src/avl_tree/main.cpp
@@ -6,9 +6,17 @@
 using namespace s21;
 
 int main() {
-	AVLTree<std::pair<AVLNode<int>, int>> tree;
-	std::cout << tree.empty();
-	std::cout << tree.size();
-	std::cout << tree.height();
+	AVLTree<std::pair<int, int>> tree;
+	tree.insert(std::make_pair(125, 118));
+	tree.insert(std::make_pair(3, 4));
+	tree.insert(std::make_pair(5, 6));
+	tree.insert(std::make_pair(7, 8));
+	tree.insert(std::make_pair(9, 10));
+	tree.insert(std::make_pair(11, 12));
+
+	tree.print();
+
+	std::cout << "\n\n\n\n" << tree.empty() << " " << tree.size() << " " << tree.height() << "\n\n\n\n";
+	tree.delete_tree();
 	return 0;
 }
