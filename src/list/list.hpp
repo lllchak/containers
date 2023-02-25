58541d9e31670c23ab97f58acbc0d3dfb3e6034a
lllchak <mediumchak@yandex.ru>
2023-02-25 16:49:50 +0300

    list - removed operator<< from class operators

--- src/list/list.hpp
+++ src/list/list.hpp
@@ -7,6 +7,28 @@
 
 
 namespace s21 {
+    
+template <typename T>
+class ListNode {
+public:
+    T data_;
+    ListNode* prev_ = nullptr;
+    ListNode* next_ = nullptr;
+
+public:
+    ListNode() = default;
+    ListNode(const T& value) : data_(value), prev_(nullptr), next_(nullptr) {}
+    ListNode(const ListNode& src) : data_(src.data_), prev_(src.prev_), next_(src.next_) {}
+    ~ListNode() = default;
+
+    ListNode& operator=(const ListNode& src) noexcept {
+        data_ = src.data_;
+        prev_ = src.prev_;
+        next_ = src.next_;
+
+        return *this;
+    }
+};
 
 template <typename T>
 class list {
@@ -18,36 +40,16 @@ public:
     using size_type = std::size_t;
 
 private:
-    class ListNode {
-    public:
-        value_type data_;
-        ListNode* prev_ = nullptr;
-        ListNode* next_ = nullptr;
-
-    public:
-        ListNode() = default;
-        ListNode(const value_type& value) : data_(value), prev_(nullptr), next_(nullptr) {}
-        ListNode(const ListNode& src) : data_(src.data_), prev_(src.prev_), next_(src.next_) {}
-        ~ListNode() = default;
-
-        ListNode& operator=(const ListNode& src) noexcept {
-            data_ = src.data_;
-            prev_ = src.prev_;
-            next_ = src.next_;
-
-            return *this;
-        }
-    };
-
-private:
-    ListNode* head_ = nullptr;
-    ListNode* tail_ = nullptr;
+    ListNode<T>* head_ = nullptr;
+    ListNode<T>* tail_ = nullptr;
     size_type size_ = 0;
-
 
 public:
     list() = default;
     ~list() { clear(); }
+
+    ListNode<T>* get_head(void) const { return head_; }
+    ListNode<T>* get_tail(void) const { return tail_; }
 
     list<value_type>& operator=(list<value_type> src) {
         size_ = src.size_;
@@ -57,13 +59,10 @@ public:
         return *this;
     }
 
-    template <typename U>
-    friend std::ostream& operator<<(std::ostream& stream, const list<U>& head);
-
 public:
 
     void push_front(const_reference data) {
-        ListNode* new_node = new ListNode(data);
+        ListNode<T>* new_node = new ListNode(data);
         if (!head_) head_ = tail_ = new_node;
         else {
             new_node->next_ = head_;
@@ -75,7 +74,7 @@ public:
     }
 
     void push_back(const_reference data) {
-        ListNode* new_node = new ListNode(data);
+        ListNode<T>* new_node = new ListNode(data);
         if (!head_) head_ = tail_ = new_node;
         else {
             tail_->next_ = new_node;
@@ -91,9 +90,9 @@ public:
 
 private:
     void clear() {
-        ListNode* curr = head_;
+        ListNode<T>* curr = head_;
         while (curr) {
-            ListNode* next = curr->next_;
+            ListNode<T>* next = curr->next_;
             delete curr;
             curr = next;
         }
@@ -101,32 +100,6 @@ private:
 
 };
 
-template <typename T>
-std::ostream& operator<<(std::ostream& stream, const list<T>& head) {
-    if (head.empty()) {
-        stream << "List is empty";
-        return stream;
-    }
-
-    ListNode* curr = head.head_;
-    stream << "NULL <-> " << curr->data_ << " <-> ";
-    curr = curr->next_;
-
-    while (curr && curr->next_) {
-        if (curr->prev_->next_->data_ == curr->next_->prev_->data_) {
-            stream << curr->data_ << " <-> ";
-        } else {
-            stream << "Invalid list";
-            return stream;
-        }
-        curr = curr->next_;
-    }
-
-    if (curr) stream << curr->data_ << " <-> ";
-    stream << "NULL" << std::endl;
-
-    return stream;
-}
 
 }  // namespace s21
 
